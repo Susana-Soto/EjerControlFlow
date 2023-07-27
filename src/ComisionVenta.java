858d26c357d19f1264f2bed313cbf772d6fd9825
Susana <susoto0916@gmail.com>
2023-07-26 19:35:54 -0600

    se corrigen errores de codigo de  cliclos if

--- src/ComisionVenta.java
+++ src/ComisionVenta.java
@@ -3,36 +3,20 @@ import java.util.Scanner;
 public class ComisionVenta {
 
     public static void main(String[] args) {
-        // Crear un objeto Scanner para leer la entrada del usuario
         Scanner scanner = new Scanner(System.in);
-
-        // Pedir al usuario que ingrese la cifra de ventas
         System.out.print("Ingrese la cifra de ventas: ");
         double ventas = scanner.nextDouble();
-
-        // Calcular la comisión según el rango de ventas
-        double comision = calcularComision(ventas);
-
-        // Mostrar la comisión calculada al usuario
-        if (comision == -1) {
-            System.out.println("El rango de ventas no es válido. Comisión no aplicable.");
+                
+        if (ventas < 1000) {
+			System.out.printf("No aplica comisión. Sigue esforzandote.");
+        } else if (ventas >= 1000 && ventas<=4999) {
+        	System.out.printf("La comisión es de:"+ ventas*0.10);
+        } else if (ventas >= 5000 && ventas<=9999) {
+        	System.out.printf("La comisión es de:"+ ventas*0.20);
+        } else if (ventas >= 10000) {
+    	System.out.printf("La comisión es de:"+ ventas*0.30);
         } else {
-            System.out.println("La comisión es del " + comision + "%.");
-        }
-
-        // Cerrar el objeto Scanner
-        scanner.close();
-    }
-
-    public static double calcularComision(double ventas) {
-        if (ventas > 10000) {
-            return 30.0;
-        } else if (ventas >= 5001 && ventas <= 9999) {
-            return 20.0;
-        } else if (ventas >= 1001 && ventas <= 4999) {
-            return 10.0;
-        } else {
-            return -1; // El rango de ventas no es válido, no hay comisión aplicable.
-        }
+        	System.out.printf("Error");
+        } 
     }
 }
--- src/PromoPeli.java
+++ src/PromoPeli.java
@@ -1,49 +1,20 @@
 import java.util.Scanner;
 
 public class PromoPeli {
-
-    public static void main(String[] args) {
-         Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese su edad: ");
-        int edad = scanner.nextInt();
-
-         double precio = calcularPrecioBoleto(edad);
-
-            if (precio == -1) {
-            System.out.println("Lo siento, no se puede calcular el precio del boleto.");
-        } else {
-            System.out.println("El precio del boleto es de " + precio + " Euros.");
-        }
-        scanner.close();
-    }
-
-    public static double calcularPrecioBoleto(int edad) {
-        double precioNormal = 7.0;
-
-        if (edad < 5) {
-            return precioNormal * 0.4;
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Escribe la edad del usuario:");
+		int edad = in.nextInt();
+		String edadString="";
+		//Hasta aquí se pide al usuario que ingrese un dato por teclado
+		if (edad < 5) {
+			System.out.printf("Descuento de 60%, precio:"+ 140*.4);
         } else if (edad >= 60) {
-           
-            return precioNormal * 0.45;
+        	System.out.printf("Descuento de 60%, precio:"+ 140*.45);
         } else {
-           
-            return precioNormal;
-        }
-    }
+        	System.out.printf("Precio normal, precio:"+ 140);
+        } 
+		in.close();
 }
-	        scanner.close();
-	    }
+}
 
-	    public static double calcularPrecioBoleto(int edad) {
-	        double precioNormal = 7.0;
-
-	        if (edad < 5) {
-	      
-	            return precioNormal * 0.4;
-	        } else if (edad >= 60) {
-	            	            return precioNormal * 0.45;
-	        } else {
-	           	            return precioNormal;
-	        }
-	    }
-	}
